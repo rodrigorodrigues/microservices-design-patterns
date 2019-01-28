d9a05abfd65025451df270851c53641721f06aae
rodrigorodriguesweb@gmail.com <rodrigorodriguesweb@gmail.com>
2019-01-28 22:42:59 +0000

    Changed Edge Configuration

--- edge-server/src/main/java/com/springboot/edgeserver/EdgeServerApplication.java
+++ edge-server/src/main/java/com/springboot/edgeserver/EdgeServerApplication.java
@@ -2,10 +2,10 @@ package com.springboot.edgeserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulServer
+@EnableZuulProxy
 public class EdgeServerApplication {
 
 	public static void main(String[] args) {
