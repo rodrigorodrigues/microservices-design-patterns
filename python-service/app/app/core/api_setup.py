7d530842618d9e56e2e68b26db607f3195c4054d
Rodrigo Rodrigues dos Santos <rodrigorodriguesweb@gmail.com>
2020-01-12 00:42:12 +0000

    Fixed swagger issue, [deploy python-service]

--- python-service/app/app/core/api_setup.py
+++ python-service/app/app/core/api_setup.py
@@ -1,4 +1,17 @@
+from flask import url_for
 from flask_restplus import Api
+
+
+class CustomApi(Api):
+    @property
+    def specs_url(self):
+        """
+        The Swagger specifications absolute url (ie. `swagger.json`)
+
+        :rtype: str
+        """
+        return url_for(self.endpoint('specs'), _external=False)
+
 
 authorizations = {
     'apikey': {
@@ -10,7 +23,7 @@ authorizations = {
 
 
 def initialize_api(app):
-    return Api(app=app, catch_all_404s=True, version='1.0', title='API - Products Service',
-          description='Products Management', doc='/swagger-ui.html',
-          default_label='products endpoints', default='products',
-          authorizations=authorizations, security='apikey')
+    return CustomApi(app=app, catch_all_404s=True, version='1.0', title='API - Products Service',
+                     description='Products Management', doc='/swagger-ui.html',
+                     default_label='products endpoints', default='products',
+                     authorizations=authorizations, security='apikey')
