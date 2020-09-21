23fd867017094100ddf2b23384ea9ed5d15541ba
Rodrigo Rodrigues dos Santos <rodrigorodriguesweb@gmail.com>
2020-09-21 23:22:22 +0100

    Add JWT validation for Go - WIP

--- go-service/cmd/main.go
+++ go-service/cmd/main.go
@@ -2,92 +2,106 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
-	"net/http"
-	"strconv"
-
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type (
-	user struct {
+	post struct {
 		ID   int    `json:"id"`
 		Name string `json:"name"`
 	}
 )
 
 var (
-	users = map[int]*user{}
+	posts = map[int]*post{}
 )
 
 //----------
 // Handlers
 //----------
-func createDefaultUsers() {
-	u := &user{
+func createDefaultPosts() {
+	u := &post{
 		ID: 1,
 		Name: "Golang",
 	}
-	users[u.ID] = u
-	u = &user{
+	posts[u.ID] = u
+	u = &post{
 		ID:   2,
 		Name: "Test",
 	}
-	users[u.ID] = u
+	posts[u.ID] = u
 }
 
-func getAllUsers(c echo.Context) error {
-	return c.JSON(http.StatusOK, users)
+func getAllPosts(c echo.Context) error {
+	return c.JSON(http.StatusOK, posts)
 }
 
-func createUser(c echo.Context) error {
-	seq := len(users)+1
-	u := &user{
+func createPost(c echo.Context) error {
+	seq := len(posts)+1
+	u := &post{
 		ID: seq,
 		Name: c.FormValue("name"),
 	}
 	if err := c.Bind(u); err != nil {
 		return err
 	}
-	users[u.ID] = u
+	posts[u.ID] = u
 	return c.JSON(http.StatusCreated, u)
 }
 
-func getUser(c echo.Context) error {
+func getPost(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
-	return c.JSON(http.StatusOK, users[id])
+	return c.JSON(http.StatusOK, posts[id])
 }
 
-func updateUser(c echo.Context) error {
-	u := new(user)
+func updatePost(c echo.Context) error {
+	u := new(post)
 	if err := c.Bind(u); err != nil {
 		return err
 	}
 	id, _ := strconv.Atoi(c.Param("id"))
-	users[id].Name = u.Name
-	return c.JSON(http.StatusOK, users[id])
+	posts[id].Name = u.Name
+	return c.JSON(http.StatusOK, posts[id])
 }
 
-func deleteUser(c echo.Context) error {
+func deletePost(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
-	delete(users, id)
+	delete(posts, id)
 	return c.NoContent(http.StatusNoContent)
 }
 
 func main() {
 	e := echo.New()
+	e.Logger.SetLevel(log.DEBUG)
 
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	bytes, err := ioutil.ReadFile(os.Getenv("PUBLIC_KEY_PATH"))
+	if err != nil {
+		panic(err)
+	}
+	publicKey := strings.Replace(string(bytes), "-----BEGIN PUBLIC KEY-----", "", -1)
+	publicKey = strings.Replace(publicKey, "-----END PUBLIC KEY-----", "", -1)
+	e.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+		SigningKey:    []byte(strings.TrimSpace(publicKey)),
+		SigningMethod: "RS256",
+	}))
 
-	createDefaultUsers()
+	createDefaultPosts()
 
 	// Routes
-	e.GET("/users", getAllUsers)
-	e.POST("/users", createUser)
-	e.GET("/users/:id", getUser)
-	e.PUT("/users/:id", updateUser)
-	e.DELETE("/users/:id", deleteUser)
+	e.GET("/posts", getAllPosts)
+	e.POST("/posts", createPost)
+	e.GET("/posts/:id", getPost)
+	e.PUT("/posts/:id", updatePost)
+	e.DELETE("/posts/:id", deletePost)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
