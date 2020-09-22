41e97ceaf821bc3b6e91ed40876788a3b4848b58
Rodrigo Rodrigues dos Santos <rodrigorodriguesweb@gmail.com>
2020-09-22 23:59:33 +0100

    Add eureka client and spring cloud config for go-service - WIP

--- go-service/cmd/main.go
+++ go-service/cmd/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"github.com/ArthurHlt/go-eureka-client/eureka"
+	"github.com/Piszmog/cloudconfigclient"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
@@ -20,6 +25,7 @@ type (
 
 var (
 	posts = map[int]*post{}
+	echoRestApi = echo.New()
 )
 
 //----------
@@ -76,33 +82,90 @@ func deletePost(c echo.Context) error {
 	return c.NoContent(http.StatusNoContent)
 }
 
-func main() {
-	e := echo.New()
-	e.Logger.SetLevel(log.DEBUG)
+func getEnv(key string, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
 
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	bytes, err := ioutil.ReadFile(os.Getenv("PUBLIC_KEY_PATH"))
+	return defaultVal
+}
+
+func getEnvAsInt(name string, defaultVal int) int {
+	valueStr := getEnv(name, "")
+	if value, err := strconv.Atoi(valueStr); err == nil {
+		return value
+	}
+
+	return defaultVal
+}
+
+func init()  {
+	// loads values from .env into the system
+	if err := godotenv.Load("./go-service/.env"); err != nil {
+		panic(err)
+	}
+
+	client := eureka.NewClient([]string{
+		getEnv("EUREKA_SERVER", "http://127.0.0.1:8761/eureka"), //From a spring boot based eureka server
+		// add others servers here
+	})
+	appId := getEnv("APP_ID", "go-service")
+	instance := eureka.NewInstanceInfo(getEnv("HOSTNAME", "localhost"),
+		appId,
+		getEnv("IP_ADDRESS", "0.0.0.0"),
+		getEnvAsInt("SERVER_PORT", 9091), 30, false) //Create a new instance to register
+	instance.Metadata = &eureka.MetaData{
+		Map: make(map[string]string),
+	}
+	client.RegisterInstance(appId, instance)
+
+	springConfigUrl := getEnv("SPRING_CLOUD_CONFIG_URI", "http://localhost:8888")
+	springConfigUrl = fmt.Sprintf("%v?X-Encrypt-Key=%v", springConfigUrl, getEnv("X_ENCRYPT_KEY", "test"))
+	configClient, err := cloudconfigclient.NewLocalClient(&http.Client{}, []string{springConfigUrl})
+
 	if err != nil {
 		panic(err)
 	}
-	publicKey := strings.Replace(string(bytes), "-----BEGIN PUBLIC KEY-----", "", -1)
-	publicKey = strings.Replace(publicKey, "-----END PUBLIC KEY-----", "", -1)
-	e.Use(middleware.JWTWithConfig(middleware.JWTConfig{
-		SigningKey:    []byte(strings.TrimSpace(publicKey)),
-		SigningMethod: "RS256",
-	}))
 
-	createDefaultPosts()
+	profiles := getEnv("SPRING_PROFILES_ACTIVE", "dev")
+	config, err := configClient.GetConfiguration(appId, []string{profiles})
+	log.Infof("CONFIG Client = %+v", config)
+
+	//JWT
+	if strings.Contains(profiles, "prod") {
+		bytes, err := ioutil.ReadFile(getEnv("PUBLIC_KEY_PATH", "/tmp/public.key"))
+		if err != nil {
+			panic(err)
+		}
+		pem, err := jwt.ParseRSAPublicKeyFromPEM(bytes)
+		if err != nil {
+			panic(err)
+		}
+		middleware.JWTWithConfig(middleware.JWTConfig{
+			SigningKey:    pem,
+			SigningMethod: "RS256",
+		})
+	} else {
+		//echoRestApi.Use(middleware.JWT([]byte(config.PropertySources[0].Source(map[string]interface{})["foo"])))
+	}
+
+	// Middleware
+	echoRestApi.Use(middleware.Recover(),
+		middleware.Logger(),
+		middleware.CSRF())
 
 	// Routes
-	e.GET("/posts", getAllPosts)
-	e.POST("/posts", createPost)
-	e.GET("/posts/:id", getPost)
-	e.PUT("/posts/:id", updatePost)
-	e.DELETE("/posts/:id", deletePost)
+	echoRestApi.Logger.SetLevel(log.DEBUG)
+	echoRestApi.GET("/posts", getAllPosts)
+	echoRestApi.POST("/posts", createPost)
+	echoRestApi.GET("/posts/:id", getPost)
+	echoRestApi.PUT("/posts/:id", updatePost)
+	echoRestApi.DELETE("/posts/:id", deletePost)
+}
+
+func main() {
+	createDefaultPosts()
 
 	// Start server
-	e.Logger.Fatal(e.Start(":1323"))
+	echoRestApi.Logger.Fatal(echoRestApi.Start(":"+getEnv("SERVER_PORT", "9091")))
 }
--- go-service/go.mod
+++ go-service/go.mod
@@ -2,4 +2,11 @@ module go-service
 
 go 1.15
 
-require github.com/labstack/echo/v4 v4.1.17 // indirect
+require (
+	github.com/ArthurHlt/go-eureka-client v1.1.0
+	github.com/Piszmog/cloudconfigclient v1.6.0
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/joho/godotenv v1.3.0
+	github.com/labstack/echo/v4 v4.1.17
+	github.com/labstack/gommon v0.3.0
+)
