2a7f2bc932e8395a55a7f46a286dbf0c8ab55a81
Rodrigo <rodrigorodriguesweb@gmail.com>
2023-10-08 02:22:37 +0100

    Fixed login issue - build authentication-service

--- authentication-service/src/main/java/com/microservice/authentication/config/SpringSecurityLoginPageConfiguration.java
+++ authentication-service/src/main/java/com/microservice/authentication/config/SpringSecurityLoginPageConfiguration.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
@@ -19,6 +20,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 @AllArgsConstructor
 @Order(303)
+@Profile("!prod")
 public class SpringSecurityLoginPageConfiguration {
     private final CustomOidcUserService customOidcUserService;
 
