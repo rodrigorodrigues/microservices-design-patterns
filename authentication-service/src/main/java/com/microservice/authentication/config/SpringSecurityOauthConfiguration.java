285bf34bb08aa4006237a383f6e1b5b3f8818079
Rodrigo <rodrigorodriguesweb@gmail.com>
2023-10-11 22:41:18 +0100

    Fixed login issue - build authentication-service

--- authentication-service/src/main/java/com/microservice/authentication/config/SpringSecurityOauthConfiguration.java
+++ authentication-service/src/main/java/com/microservice/authentication/config/SpringSecurityOauthConfiguration.java
@@ -1,14 +1,11 @@
 package com.microservice.authentication.config;
 
-import com.microservice.authentication.service.CustomAuthenticationSuccessHandler;
-import com.microservice.authentication.service.CustomOidcUserService;
 import lombok.AllArgsConstructor;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,25 +14,17 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
 /**
  * Spring Security Configuration for oauth2
  */
 @ConditionalOnProperty(prefix = "oauth", name = "enabled", havingValue = "true", matchIfMissing = true)
-//@Configuration
+@Configuration
 @EnableWebSecurity
 @AllArgsConstructor
 @Order(301)
 public class SpringSecurityOauthConfiguration {
-
-    private final CustomOidcUserService customOidcUserService;
-
-    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
-
-    private final LogoutSuccessHandler customLogoutSuccessHandler;
 
     @Bean
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -55,56 +44,5 @@ public class SpringSecurityOauthConfiguration {
                 .jwt(Customizer.withDefaults()))
             .build();
     }
-
-   /* @Bean
-    public SecurityFilterChain securityFilterChainOauth(HttpSecurity http) throws Exception {
-        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-
-        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-            .oidc(Customizer.withDefaults());
-        return http
-//            .securityMatcher("/oauth2/**", "/oauth2logout", "/login/**")
-//            .authorizeHttpRequests(a -> a.anyRequest().authenticated())
-//            .authorizeHttpRequests(a -> a.requestMatchers("/oauth2/**", "/logout", "/login/**").authenticated())
-            .logout(l -> l.logoutSuccessUrl("/logout")
-                .deleteCookies("SESSIONID")
-                .logoutSuccessHandler(customLogoutSuccessHandler)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", HttpMethod.GET.name()))
-                .invalidateHttpSession(true))
-            .oauth2Login(o -> o.successHandler(customAuthenticationSuccessHandler)
-                .userInfoEndpoint(u -> u.oidcUserService(customOidcUserService)))
-            // Redirect to the login page when not authenticated from the
-            // authorization endpoint
-            .exceptionHandling(exceptions -> exceptions
-                .defaultAuthenticationEntryPointFor(
-                    new LoginUrlAuthenticationEntryPoint("/login"),
-                    new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
-                )
-            )
-            // Accept access tokens for User Info and/or Client Registration
-            .oauth2ResourceServer(resourceServer -> resourceServer
-                .jwt(Customizer.withDefaults()))
-*//*
-            .getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-            .oidc(Customizer.withDefaults())
-            .and()
-*//*
-            .build();
-
-        *//*return http.authorizeHttpRequests()
-            .requestMatchers("/oauth2/**", "/logout", "/login/**")
-            .authenticated()
-            .and().logout()
-                .logoutSuccessUrl("/logout")
-                .deleteCookies("SESSIONID")
-                .logoutSuccessHandler(customLogoutSuccessHandler)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", HttpMethod.GET.name()))
-                .invalidateHttpSession(true)
-            .and().oauth2Login()
-                .successHandler(customAuthenticationSuccessHandler)
-                .userInfoEndpoint()
-                .oidcUserService(customOidcUserService)
-            .and().and().build();*//*
-    }*/
 
 }
