6c693baadef56ae5d4bdf13fea8b80e8d3976148
Rodrigo <rodrigorodriguesweb@gmail.com>
2023-10-18 23:25:33 +0100

    Fixed oauth2 login, build authentication-service

--- authentication-service/src/main/java/com/microservice/authentication/config/SpringSecurityLoginPageConfiguration.java
+++ authentication-service/src/main/java/com/microservice/authentication/config/SpringSecurityLoginPageConfiguration.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
@@ -20,7 +19,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 @AllArgsConstructor
 @Order(303)
-@Profile("!prod")
 public class SpringSecurityLoginPageConfiguration {
     private final CustomOidcUserService customOidcUserService;
 
