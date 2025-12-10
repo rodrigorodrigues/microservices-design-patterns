7b47b45e1592bdd7b4262897ff90ba5c60419c03
Rodrigo <rodrigorodriguesweb@gmail.com>
2025-12-10 23:29:47 +0000

    Update spring boot 4 - build kotlin-service

---
+++ kotlin-service/src/test/kotlin/com/microservice/kotlin/TestcontainersConfiguration.kt
@@ -0,0 +1,16 @@
+package com.microservice.kotlin
+
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.context.annotation.Bean
+import org.testcontainers.mongodb.MongoDBContainer
+import org.testcontainers.utility.DockerImageName
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+    @Bean
+    @ServiceConnection
+    fun mongoDbContainer(): MongoDBContainer {
+        return MongoDBContainer(DockerImageName.parse("mongo:latest"))
+    }
+}
